50f0aa2cdf0e3a5545a24d473dd93b3cca738df4
w5-zy <1838452424@qq.com>
2026-08-12 10:20:38 +0800

    Remove User and Goods models from backend

--- backend/models.py
+++ backend/models.py
@@ -1,19 +1,1 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)
-    goods = relationship("Goods", back_populates="owner")
-
-class Goods(Base):
-    __tablename__ = "goods"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False)
-    price = Column(Integer, nullable=False)
-    desc = Column(Text)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    owner = relationship("User", back_populates="goods")

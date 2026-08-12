2428cab973ef6a3f523cf533d3597dbba5cdbedc
w5-zy <1838452424@qq.com>
2026-08-12 10:26:05 +0800

    Update index.js

--- backend/frontend/src/router/index.js
+++ backend/frontend/src/router/index.js
@@ -1,18 +1,1 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/Login.vue'
-import Home from '../views/Home.vue'
-import Publish from '../views/Publish.vue'
-import MyGoods from '../views/MyGoods.vue'
 
-const routes = [
-  {path:'/',component:Home},
-  {path:'/login',component:Login},
-  {path:'/publish',component:Publish},
-  {path:'/mygoods',component:MyGoods}
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
-export default router
